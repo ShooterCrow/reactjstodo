ebfd394b68a0a8a69a66787498721d70eb5fdd48
Victor Onyekwere <cydollarsign1@gmail.com>
2024-08-13 04:11:48 +0200

    Been a long time, Busy with school work. Fixing notice display on first load

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -3,14 +3,26 @@ import '../Components/CSS/Info.css'
 
 const Info = ({ todos, displayPop, infoRef, infoTitle, infoDetails, popControl }) => {
 
+  // const [sp, setSp] = useState(false)
+  const [firstLoad, setFirstLoad] = useState(true)
+
   infoRef = useRef(null)
 
   const lastTodoAdded = () => {
     return todos.length===0 ? "No Tasks Was Set" : (todos[todos.length - 1])
   }
+  
+  // useEffect(() => {
+  //   !
+  // })
 
   useEffect(() => {
-    displayPop(infoRef.current)
+    if (firstLoad) {
+      displayPop(infoRef.current)
+    } else {
+      setFirstLoad(false)
+      return
+    }
   }, [todos, popControl])
 
   return (
